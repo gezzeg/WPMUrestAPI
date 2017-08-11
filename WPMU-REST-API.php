3b16deeec3ae76e3e03d627116b536bd5f7deb2b
Gezzeg <mr.ghazali@gmail.com>
2017-08-11 16:51:02 +0800

    Add files via upload

---
+++ WPMU-REST-API.php
@@ -0,0 +1,71 @@
+<?php
+/*
+* Plugin Name: Wordpress REST API Multisite
+* Plugin URI: 
+* Description: 
+* Version: 1.0
+* Author: Ghazali Tajuddin
+* Author URI: http://www.ghazalitajuddin.com
+* License: MIT
+* */
+
+
+class WPMUrestAPI{
+
+    function __construct() {
+        add_action( 'rest_api_init', array( $this, 'call2' ));
+    }
+    
+
+    public function call2() {
+    // Here we are registering our route for a collection of products and creation of products.
+    
+        register_rest_route( 'all/sites', '/list', 
+            array(
+                
+                // By using this constant we ensure that when the WP_REST_Server changes, our readable endpoints will work as intended.
+                
+                'methods'  => WP_REST_Server::READABLE,
+                
+                // Here we register our callback. The callback is fired when this endpoint is matched by the WP_REST_Server class.
+                
+                'callback' => array($this,'get_sites123'),
+            )
+         );
+    }
+
+   public function prefix_get_endpoint_phrase() {
+        // rest_ensure_response() wraps the data we want to return into a WP_REST_Response, and ensures it will be properly returned.
+        //return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+        return 'Hello World, this is the WordPress REST API';
+    }
+
+    public function get_sites123() {
+            
+            if ( function_exists( 'get_sites' )  ) {
+            $sites = get_sites( 
+                [
+                    'public'  => 1,
+                    //'number'  => 500,
+                    'orderby' => 'registered',
+                    'order'   => 'DESC',
+                ]
+            );
+
+            $sites_details = array();
+
+            foreach ($sites as $site) {
+                $sites_details[] = $site->blog_id;
+            }
+
+            return $sites_details;
+ 
+    }
+}
+
+  
+
+}
+
+
+ $power = new WPMUrestAPI();
